4a1382333e75d25927437365b3afc9fe853245ba
maxim akimov <72648180+drevniyucr@users.noreply.github.com>
2026-07-03 16:49:33 +0300

    /

--- firmware/Project/CAN.c
+++ firmware/Project/CAN.c
@@ -1,20 +1,20 @@
 // =============================================================================
 // CAN.c — передача кадров по CAN0 (модуль работает только на отправку).
 // =============================================================================
-#include "gd32f10x.h"
 #include "CAN.h"
+#include "gd32f10x.h"
 
-void CAN_send_arr(unsigned char sfid, unsigned char* msg, unsigned char qnt)
-{
-    can_trasnmit_message_struct transmit_message;
-    unsigned char i;
+void CAN_send_arr(unsigned char sfid, unsigned char *msg, unsigned char qnt) {
+  can_trasnmit_message_struct transmit_message;
+  unsigned char i;
 
-    transmit_message.tx_sfid = sfid;
-    transmit_message.tx_efid = 0x00;
-    transmit_message.tx_ff = CAN_FF_STANDARD;
-    transmit_message.tx_ft = CAN_FT_DATA;
-    transmit_message.tx_dlen = qnt;
-    for (i = 0; i < qnt; i++) transmit_message.tx_data[i] = msg[i];
+  transmit_message.tx_sfid = sfid;
+  transmit_message.tx_efid = 0x00;
+  transmit_message.tx_ff = CAN_FF_STANDARD;
+  transmit_message.tx_ft = CAN_FT_DATA;
+  transmit_message.tx_dlen = qnt;
+  for (i = 0; i < qnt; i++)
+    transmit_message.tx_data[i] = msg[i];
 
-    can_message_transmit(CAN0, &transmit_message);
+  can_message_transmit(CAN0, &transmit_message);
 }
